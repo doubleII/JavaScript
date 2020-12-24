d7a10d943f21385d9d903adf80f77831bd32c9bb
doubleII <vonavii@yahoo.com>
2020-12-24 15:47:37 +0100

    added json header

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
+/*
 const requestListener = function (req, res) {
 	res.setHeader("Content-Type", "application/json");
     res.writeHead(200);
@@ -10,6 +11,14 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
+*/
+
+const server = http.createServer(function (req, res) {
+	res.setHeader("Content-Type", "application/json");
+    res.writeHead(200); // code 200 site is found
+    res.end(`{"message": "This is a JSON response"}`);
+});
+
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
